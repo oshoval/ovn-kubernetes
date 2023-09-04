2ed7763e6efd948f950527ae2702d8478e154ca0
Or Shoval <oshoval@redhat.com>
2023-09-04 11:04:36 +0300

    Revert "multi-homing, e2e tests: wait for NAD creation to settle"
    This reverts commit c2d6de7095769c439612e48928436e0a6577fdeb.
    
    Signed-off-by: Or Shoval <oshoval@redhat.com>

--- test/e2e/multihoming.go
+++ test/e2e/multihoming.go
@@ -875,10 +875,6 @@ var _ = Describe("Multi Homing", func() {
 						Expect(err).NotTo(HaveOccurred())
 					}
 
-					By("sitting on our hands for a couple secs we give the controller time to sync all NADs before provisioning policies and pods")
-					// TODO: this is temporary. We hope to eventually sync pods & multi-net policies on NAD C/U/D ops
-					time.Sleep(3 * time.Second)
-
 					kickstartPod(cs, serverPodConfig)
 					kickstartPod(cs, allowedClientPodConfig)
 					kickstartPod(cs, blockedClientPodConfig)
@@ -1282,10 +1278,6 @@ var _ = Describe("Multi Homing", func() {
 				)
 				Expect(err).NotTo(HaveOccurred())
 			}
-
-			By("sitting on our hands for a couple secs we give the controller time to sync all NADs before provisioning policies and pods")
-			// TODO: this is temporary. We hope to eventually sync pods & multi-net policies on NAD C/U/D ops
-			time.Sleep(3 * time.Second)
 
 			podConfig := podConfiguration{
 				attachments: []nadapi.NetworkSelectionElement{
